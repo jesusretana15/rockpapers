8fb766cb25c3c101bbef8004c5a7063ee923b93f
Jesus Retana Artavia <jesus@Jesuss-MacBook-Air.local>
2022-09-29 01:03:47 -0600

    game

---
+++ game.js
@@ -0,0 +1,116 @@
+
+let userPointsHolder = document.getElementById("points-user");
+let computerPointsHolder = document.getElementById("points-pc");
+let rockbtn = document.getElementsByClassName("rockpc");
+let paperbtn = document.getElementsByClassName("paperpc");
+let scissorsbtn = document.getElementsByClassName("scissorspc");
+
+function startGame() {
+    console.log('hehe')    
+    const choiceBtns = document.querySelectorAll(".choicebtn");
+    let computer;
+    let player;
+    let point;
+    let computerPoints = 0;
+    let userPoints = 0;
+    assingPoints(computerPoints,userPoints);  
+    choiceBtns.forEach(button => button.addEventListener("click" , () => {
+        deselectPC(computer);
+        player = button.name;
+        computer = getComputerAnswer();
+        selectPC(computer);
+        point = playRound(player,computer);
+        if (point == "player") {
+                userPoints= userPoints +1;
+            } else if (point == "computer"){
+                computerPoints= computerPoints+1;
+            } else {}
+        
+        assingPoints(computerPoints,userPoints);
+        
+        if ((computerPoints == 5 ) || (userPoints == 5)) {
+            if (computerPoints == 5) {
+                alert("pc won")
+            } else {
+                alert("you won")
+            }
+            location.href = "../index.html";
+            
+        }
+
+    }));
+
+    
+}
+
+
+function selectPC(answer) {
+    if (answer == "rock") {
+        document.getElementById("rockpc").setAttribute("class", "selected"); 
+ 
+    } else if (answer == "paper") {
+        document.getElementById("paperpc").setAttribute("class", "selected"); 
+    } else {
+        document.getElementById("scissorspc").setAttribute("class", "selected"); 
+    }  
+
+}
+
+function deselectPC(answer) {
+    if (answer == "rock") {
+        document.getElementById("rockpc").removeAttribute("class", "selected"); 
+ 
+    } else if (answer == "paper") {
+        document.getElementById("paperpc").removeAttribute("class", "selected"); 
+    } else {
+        document.getElementById("scissorspc").removeAttribute("class", "selected"); 
+    }  
+    
+}
+
+function playRound(playerSelection, computerSelection) {
+    let result = "";
+    if (playerSelection == computerSelection) {
+        console.log("There is a tie!!");
+        result = "tie";        
+        return result;
+    } else if ((playerSelection == "rock" && computerSelection == "scissors")|| (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock")){
+        result = "player";
+        console.log("You Won! " + playerSelection  +" beats " + computerSelection  );
+    } else{
+        result = "computer";
+        console.log("You Lose! " + computerSelection +" beats " + playerSelection  );
+    }
+    return result;  
+    }
+
+    function assingPoints(pcPoints, userPoints) {
+        userPointsHolder.textContent = userPoints.toString();
+        computerPointsHolder.textContent = pcPoints.toString();
+    } 
+
+function getComputerAnswer(){
+    let number = Math.floor(Math.random() * 3);
+    let answer ="";
+    switch (number) {
+        case 0:
+            answer = "rock"
+            break;
+
+        case 1:
+            answer = "paper"
+            break;
+        
+        case 2:
+            answer = "scissors"
+            break;    
+    
+        default:
+            break;
+    }
+    return answer; 
+}
+
+
+
+startGame();
--- index.js
+++ index.js
@@ -4,76 +4,34 @@ console.log("Hello!")
 // let option1 = "paper";
 // let option2 = "scissor";
 
-function getComputerChoice(){
-    let number = Math.floor(Math.random() * 3);
-    let answer ="";
-    switch (number) {
-        case 0:
-            answer = "rock"
-            break;
+// function startGame(){
+//     let userPoints = 0;
+//     let computerPoints = 0;
+//     for (let i = 0; i < 5; i++) {
+//         let point = "";
+//         let userSelection = getplayerSelection();
+//         let computerSelection = getComputerAnswer();
+//         point = playRound(userSelection,computerSelection);
+//         if (point == "player") {
+//             userPoints= userPoints +1;
+//         } else if (point == "computer"){
+//             computerPoints= computerPoints+1;
+//         }
+//         console.log("Player point: " + userPoints + " vs Computer points: " + computerPoints)
+//      }
 
-        case 1:
-            answer = "paper"
-            break;
-        
-        case 2:
-            answer = "scissors"
-            break;    
-    
-        default:
-            break;
-    }
-    return answer; 
-}
+// }
 
 
 
 
-function getplayerSelection(){
-    let answer = "";
-    do {
-        answer = prompt("Please choose; rock, paper, scissors"); 
-        answer = answer.toLowerCase(); 
-    } while (answer != "rock" && answer != "paper" && answer != "scissors");
-   
-    return answer;
-}
+
+ 
+document.getElementById("bttn-start").onclick = function () {
+    location.href = "../game.html";
+};
 
 
 
-function playRound(playerSelection, computerSelection) {
-    let result = "";
-    if (playerSelection == computerSelection) {
-        console.log("There is a tie!!");
-        result = "tie";
-        
-        return result;
-    } else if ((playerSelection == "rock" && computerSelection == "scissors")|| (playerSelection == "scissors" && computerSelection == "papper") || (playerSelection == "paper" && computerSelection == "scissrockors")){
-        result = "player";
-        console.log("You Won! " + playerSelection  +" beats " + computerSelection  );
-    } else{
-        result = "computer";
-        console.log("You Lose! " + computerSelection +" beats " + playerSelection  );
-    }
-    return result;  
-    }
 
-function game(){
-    
-    let userPoints = 0;
-    let computerPoints = 0;
-    for (let i = 0; i < 5; i++) {
-        let point = "";
-        let userSelection = getplayerSelection();
-        let computerSelection = getComputerChoice();
-        point = playRound(userSelection,computerSelection);
-        if (point == "player") {
-            userPoints= userPoints +1;
-        } else if (point == "computer"){
-            computerPoints= computerPoints+1;
-        }
-        console.log("Player point: " + userPoints + " vs Computer points: " + computerPoints)
-     }
-}
 
-game();
